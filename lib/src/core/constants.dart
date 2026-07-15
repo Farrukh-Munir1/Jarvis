86458008c2166dcd91e41f18f36d8cebabc206c1
Farrukh-Munir1 <farrukhmunir1111@gmail.com>
2026-07-15 17:15:18 +0500

    Add core constants

---
+++ lib/src/core/constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  static const String appName = 'Jarvis';
+  static const int isarSchemaVersion = 1;
+  static const String geminiEnvVar = 'GEMINI_API_KEY';
+  static const String methodChannelName = 'com.farrukh.jarvis/native';
+}
