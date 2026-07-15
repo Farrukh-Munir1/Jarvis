10da80c9a12e4e3f7e4981ec85b148b6dd2aded0
Farrukh-Munir1 <farrukhmunir1111@gmail.com>
2026-07-15 09:31:31 +0500

    Add Android Application class

---
+++ android/app/src/main/kotlin/com/farrukh/jarvis/Application.kt
@@ -0,0 +1,10 @@
+package com.farrukh.jarvis
+
+import android.app.Application
+
+class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize any native SDKs or services here if needed
+    }
+}
